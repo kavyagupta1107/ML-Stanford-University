b5e86cd8bb9adf50a85f0d424744a10c25afe30e
Kavya Gupta <35874192+kavyagupta1107@users.noreply.github.com>
2019-06-14 20:14:24 +0530

    Update featureNormalize.m

--- week 2/featureNormalize.m
+++ week 2/featureNormalize.m
@@ -9,7 +9,8 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-
+mu=mean(X);
+sigma=std(X);
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
@@ -28,7 +29,16 @@ sigma = zeros(1, size(X, 2));
 
 
 
-
+num=size(X,2);
+for f=1:num
+    temp=X(:,f);
+    k1=temp-mu(:,f);
+    k2=k1/sigma(:,f);
+    X_norm(:,f)=k2;
+    
+    
+    
+end
 
 
 
